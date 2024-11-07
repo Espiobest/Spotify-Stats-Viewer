fc19a621c1b9b5893f8eecbf8cdaab2c7a5b82a7
Espiobest <59823894+Espiobest@users.noreply.github.com>
2024-11-07 03:42:35 -0500

    fix now playing

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 import { fetchSpotifyData } from '../services/spotifyService';
 import { NowPlayingTrack } from '../types/spotify';
 import Link from 'next/link';
@@ -10,73 +9,79 @@ interface NowPlayingProps {
 }
 
 const NowPlaying: React.FC<NowPlayingProps> = ({ track }) => {
-    let trackDetails = track?.item;
+    const [nowPlayingTrack, setNowPlayingTrack] = useState<NowPlayingTrack | null>(track);
     const [progress, setProgress] = useState(0);
 
     useEffect(() => {
-        if (!track || !trackDetails) return;
-        setProgress(track.progress_ms);
+        if (!track || !track?.item) return;
+        
+        nowPlayingTrack ? setProgress(nowPlayingTrack.progress_ms) : setProgress(track.progress_ms);
 
         const interval = setInterval(() => {
-            if (!track || !trackDetails) {
-                return;
-            }
-            if (track.progress_ms < trackDetails.duration_ms) {
-                setProgress(progress => progress + 1000);
-                console.log(progress, track.progress_ms, trackDetails.duration_ms);
-            }
-            else {
-                setProgress(0);
-                fetchSpotifyData('me/player/currently-playing')
-                .then(data => {
-                    if (data) {
-                        track = data;
-                        trackDetails = data.item;
-                    }
-                });
-            }
-            
+            setProgress((prevProgress) => {
+                let newProgress = prevProgress + 1000;
+
+                if (newProgress >= track.item.duration_ms) {
+                    clearInterval(interval);
+                    setProgress(0);
+                    fetchSpotifyData('me/player/currently-playing')
+                        .then((data) => {
+                            if (data) {
+                                setNowPlayingTrack(data);
+                                setProgress(0);
+                                newProgress = 0;
+                            }
+                        });
+                }
+
+                return newProgress < track.item.duration_ms ? newProgress : prevProgress;
+            });
         }, 1000);
 
         return () => clearInterval(interval);
-            
-    }, [track]);
+    }, [nowPlayingTrack]);
 
-    if (!trackDetails) {
-        return null
+    if (!nowPlayingTrack?.item) {
+        return null;
     }
 
-    const progressPercentage = trackDetails ? (progress / trackDetails.duration_ms) * 100 : 0;
+    const trackDetails = nowPlayingTrack.item;
+    const progressPercentage = (progress / trackDetails.duration_ms) * 100;
 
     return (
-        <div className="flex items-center p-4 bg-gray-800 shadow-md">
+        <div className="bg-gray-800 shadow-md">
+            <h1 className="text-2xl text-center p-2">Listening To</h1>
+
+            <div className="flex items-center p-4">
                 <Image
                     src={trackDetails.album.images[0].url}
                     alt={trackDetails.name}
                     width={64}
                     height={64}
                     className="rounded-lg m-1"
-                ></Image>
-            <Link href={`/tracks/${trackDetails.id}`}>
-                <div className="flex flex-col">
-                    <h2 className="text-lg pl-2 font-bold text-white">{trackDetails.name}</h2>
-                    <p className="text-gray-400 pl-2 text-sm">{trackDetails.artists.map(artist => artist.name).join(', ')}</p>
+                />
+                <Link href={`/tracks/${trackDetails.id}`}>
+                    <div className="flex flex-col">
+                        <h2 className="text-lg pl-2 font-bold text-white">{trackDetails.name}</h2>
+                        <p className="text-gray-400 pl-2 text-sm">
+                            {trackDetails.artists.map((artist) => artist.name).join(', ')}
+                        </p>
+                    </div>
+                </Link>
+                <div className="flex justify-between text-sm text-gray-400 ml-10">
+                    <span>{formatTime(progress)}</span>
                 </div>
-            </Link>
-            <div className="flex justify-between text-sm text-gray-400 ml-10">
-                <span>{formatTime(progress)}</span>
-            </div>
 
-            <div className="relative right w-10/12 mr-6 left-5 h-2 bg-gray-600 rounded">
-            
-            <div
-                className="absolute top-0 h-full bg-green-500 rounded"
-                style={{ width: `${progressPercentage}%` }}
-            ></div>
-            </div>
+                <div className="relative right w-10/12 mr-6 left-5 h-2 bg-gray-600 rounded">
+                    <div
+                        className="absolute top-0 h-full bg-green-500 rounded"
+                        style={{ width: `${progressPercentage}%` }}
+                    ></div>
+                </div>
 
-            <div className="flex justify-between text-sm text-gray-400">
-                <span>{formatTime(trackDetails.duration_ms)}</span>
+                <div className="flex justify-between text-sm text-gray-400">
+                    <span>{formatTime(trackDetails.duration_ms)}</span>
+                </div>
             </div>
         </div>
     );
